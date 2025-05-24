523db18e3770cdd8d975a67f6ac7c08e6e732467
Tridib Samanta <tridibsamanta311@gmail.com>
2025-05-24 13:37:31 +0530

    feat(new): Health check controller added

---
+++ src/main/java/com/Testing/MockitoTesting/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.Testing.MockitoTesting.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok("Ok");
+    }
+}
--- src/test/java/com/Testing/MockitoTesting/TestContainerConfiguration.java
+++ src/test/java/com/Testing/MockitoTesting/TestContainerConfiguration.java
@@ -3,17 +3,24 @@ package com.Testing.MockitoTesting;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
+//import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
 public class TestContainerConfiguration {
     @Bean
     @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:8.0"));
-//                .withDatabaseName("testdb")
-//                .withUsername("testuser")
-//                .withPassword("testpass");
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"))
+                .withDatabaseName("testdb")
+                .withUsername("testuser")
+                .withPassword("testpass");
     }
+//    MySQLContainer<?> mysqlContainer() {
+//        return new MySQLContainer<>(DockerImageName.parse("mysql:8.0"));
+////                .withDatabaseName("testdb")
+////                .withUsername("testuser")
+////                .withPassword("testpass");
+//    }
 }
